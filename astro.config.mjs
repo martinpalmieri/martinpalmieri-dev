3ea32cd14345a6c85e6a83a277204f3beab9f30f
Martin Palmieri <martinpalmieri@gmail.com>
2026-06-16 11:03:04 +0200

    dependabot fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
-  integrations: [react(), tailwind()]
+  integrations: [react()]
 });
